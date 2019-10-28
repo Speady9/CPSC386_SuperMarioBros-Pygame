9e86526a8fe832ecadaed888e350bfad98309437
olivaresjarrel <olivares.jarrel@csu.fullerton.edu>
2019-10-28 01:36:20 -0700

    Update mario.py
    refined movement
    added gravity + acceleration
    sort of added jumping need to adjust more
    added a platform class in order to test movements

--- mario.py
+++ mario.py
@@ -1,16 +1,40 @@
 import pygame
+from pygame.sprite import Sprite
+vec2 = pygame.math.Vector2
 
 
-class Mario(object):
+class Mario(Sprite):
 
-    def __init__(self, x, y):
-        self.xcor = x
-        self.ycor = y
+    def __init__(self, x, y, width, height):
+        # call parent class
+        pygame.sprite.Sprite.__init__(self)
+
+        # initialize
+        self.width = width
+        self.height = height
+        self.inAir = False
+        self.crouching = False
+
+        # variables for jumping
+        self.velX, self.velY = 0, 0
+        self.vel = vec2(0, 0)
+        self.accX, self.accY = 0, 0
+        self.acc = vec2(0, 0)
+        self.fri = float(-0.30)
+
+        #  create rect
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(pygame.Color("#008080"))
+        self.rect = self.image.get_rect()
+
+        #  rect placement
+        self.pos = vec2(x, y)
+        # self.rect.centerx = x
+        # self.rect.bottom = y
+
     # load Mario
     # self.image = pygame.image.load('images/INSERT')
     # self.image = pygame.transform.scale(self.image, (10, 20))
-    rect = pygame.Rect(900/2, 600/2, 10, 20)
-    # screen_rect = screen.get_rect()
 
     # r, sr = self.rect, self.screen_rect
     #
@@ -32,9 +56,6 @@ class Mario(object):
 
     # Environment
     isInWater = False
-
-    # player move
-    moveSpeed = 2
 
     def becomeSmall(self):
         isSmall = True
@@ -60,6 +81,10 @@ class Mario(object):
         isFire = False
         isStar = True
 
+    def jump(self):
+        # need to add only jumping while standing on the ground
+        self.vel.y = float(-.25)
+
     def shootFire(self):  # make a separate class for the obj
         pass
 
@@ -81,14 +106,43 @@ class Mario(object):
     def update(self):
         r = self.rect
 
+        # movement speed
+        self.acc = vec2(0, float(.00025))  #  Y affects gravity
+
         if self.moveDown and r.bottom < 600:
-            r.centery += self.moveSpeed
+            r.centery += self.velX
 
         if self.moveUp and r.top > 0:
-            r.centery -= self.moveSpeed
+            r.centery -= self.velX
 
-        if self.moveLeft and r.left > 0:
-            r.centerx -= self.moveSpeed
+        if self.moveLeft and r.left:
+            self.acc.x = float(-0.08)
+            r.centerx -= self.velX
 
-        if self.moveRight and r.right < 900:
-            r.centerx += self.moveSpeed
+        if self.moveRight and r.right:
+            self.acc.x = float(0.08)
+            r.centerx += self.velX
+
+        # apply friction
+        self.acc.x += self.vel.x * self.fri
+        # equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + float(0.1) * self.acc
+
+        # wrap around screen
+        if self.pos.x > 890:
+            self.pos.x = 10
+        if self.pos.x < 10:
+            self.pos.x = 890
+
+        self.rect.midbottom = self.pos
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((width, height))
+        self.image.fill(pygame.Color("#800080"))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
